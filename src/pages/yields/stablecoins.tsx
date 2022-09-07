66038ed3c27ccedf3bcd306858d9adaac1b60300
Cleverdevcharles <cleverdevcharles@gmail.com>
2022-09-07 08:54:47 -0700

    Live

--- src/pages/yields/stablecoins.tsx
+++ src/pages/yields/stablecoins.tsx
@@ -1,28 +1,39 @@
-import Layout from '~/layout'
-import YieldPage from '~/components/YieldsPage'
-import { revalidate } from '~/api'
-import { getYieldPageData } from '~/api/categories/yield'
-import pako from 'pako'
+// import Layout from '~/layout'
+// import YieldPage from '~/components/YieldsPage'
+// import { revalidate } from '~/api'
+// import { getYieldPageData } from '~/api/categories/yield'
+// import pako from 'pako'
 
-export async function getStaticProps() {
-	const data = await getYieldPageData()
-	const strData = JSON.stringify(data)
+// export async function getStaticProps() {
+// 	const data = await getYieldPageData()
+// 	const strData = JSON.stringify(data)
 
-	const a = pako.deflate(strData)
-	const compressed = Buffer.from(a).toString('base64')
+// 	const a = pako.deflate(strData)
+// 	const compressed = Buffer.from(a).toString('base64')
 
-	return {
-		props: { compressed },
-		revalidate: revalidate(23)
-	}
+// 	return {
+// 		props: { compressed },
+// 		revalidate: revalidate(23)
+// 	}
+// }
+
+// export default function YieldPlots(compressedProps) {
+// 	const b = new Uint8Array(Buffer.from(compressedProps.compressed, 'base64'))
+// 	const data = JSON.parse(pako.inflate(b, { to: 'string' }))
+// 	return (
+// 		<Layout title={`Stablecoins - DAppView Yield`} defaultSEO>
+// 			<YieldPage {...data.props} />
+// 		</Layout>
+// 	)
+// }
+
+
+import React from 'react'
+
+const stablecoins = () => {
+  return (
+	<div>stablecoins</div>
+  )
 }
 
-export default function YieldPlots(compressedProps) {
-	const b = new Uint8Array(Buffer.from(compressedProps.compressed, 'base64'))
-	const data = JSON.parse(pako.inflate(b, { to: 'string' }))
-	return (
-		<Layout title={`Stablecoins - DAppView Yield`} defaultSEO>
-			<YieldPage {...data.props} />
-		</Layout>
-	)
-}
+export default stablecoins
--- src/pages/yields/watchlist.tsx
+++ src/pages/yields/watchlist.tsx
@@ -1,28 +1,37 @@
-import Layout from '~/layout'
-import { revalidate } from '~/api'
-import { getYieldPageData } from '~/api/categories/yield'
-import { YieldsWatchlistContainer } from '~/containers/Watchlist'
-import pako from 'pako'
+// import Layout from '~/layout'
+// import { revalidate } from '~/api'
+// import { getYieldPageData } from '~/api/categories/yield'
+// import { YieldsWatchlistContainer } from '~/containers/Watchlist'
+// import pako from 'pako'
 
-export async function getStaticProps() {
-	const data = await getYieldPageData()
-	const strData = JSON.stringify(data.props.pools)
+// export async function getStaticProps() {
+// 	const data = await getYieldPageData()
+// 	const strData = JSON.stringify(data.props.pools)
 
-	const a = pako.deflate(strData)
-	const compressed = Buffer.from(a).toString('base64')
+// 	const a = pako.deflate(strData)
+// 	const compressed = Buffer.from(a).toString('base64')
 
-	return {
-		props: { compressed },
-		revalidate: revalidate(23)
-	}
+// 	return {
+// 		props: { compressed },
+// 		revalidate: revalidate(23)
+// 	}
+// }
+
+// export default function Portfolio(compressedProps) {
+// 	const b = new Uint8Array(Buffer.from(compressedProps.compressed, 'base64'))
+// 	const data = JSON.parse(pako.inflate(b, { to: 'string' }))
+// 	return (
+// 		<Layout title={`Saved Pools - DAppView`} defaultSEO>
+// 			<YieldsWatchlistContainer protocolsDict={data} />
+// 		</Layout>
+// 	)
+// }
+import React from 'react'
+
+const watchlist = () => {
+  return (
+	<div>watchlist</div>
+  )
 }
 
-export default function Portfolio(compressedProps) {
-	const b = new Uint8Array(Buffer.from(compressedProps.compressed, 'base64'))
-	const data = JSON.parse(pako.inflate(b, { to: 'string' }))
-	return (
-		<Layout title={`Saved Pools - DAppView`} defaultSEO>
-			<YieldsWatchlistContainer protocolsDict={data} />
-		</Layout>
-	)
-}
+export default watchlist
